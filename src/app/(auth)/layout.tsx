34c959a98511d55cf5cb24a6924ba22e2af3b2db
linzheng <alidlg9045@126.com>
2024-10-30 19:18:31 +0800

    feat: create sign in and sign up page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname()
+  const isSignIn = pathname === '/sign-in'
+
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex items-center justify-between">
+          <Image src='/logo.svg' width={50} height={50} alt="logo" />
+          <Button asChild variant={'outline'}>
+            <Link href={isSignIn ? '/sign-up' : '/sign-in'}>
+              {isSignIn ? 'Sign Up' : 'Login'}
+            </Link>
+          </Button>
+        </nav>
+        <div className="flex items-center justify-center pt-4 md:p-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ src/features/auth/components/sign-up-card.tsx
@@ -0,0 +1,112 @@
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub } from 'react-icons/fa'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import DottedSeparator from '@/components/dotted-separator';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Link from 'next/link'
+
+
+export default function SignUpCard() {
+  const formSchema = z.object({
+    username: z.string().trim().min(1, {message: '请输入用户名'}),
+    email: z.string().email(),
+    password: z.string().min(8, {
+      message: "最少需要8位",
+    }),
+  })
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: ""
+    },
+  })
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+  return (
+    <Card className="w-full md:w-[487px] h-full border-none shadow-none">
+      <CardHeader className="p-7 text-center">
+        <CardTitle className="text-2xl"> Sign Up </CardTitle>
+      </CardHeader>
+      <DottedSeparator className='px-7 mb-2' />
+      <CardContent className='p-7'>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="用户名" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="邮箱" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="密码" {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" size='lg' className='w-full' >登录</Button>
+          </form>
+        </Form>
+      </CardContent>
+      <DottedSeparator className='px-7 mb-2' />
+      <CardContent className='flex flex-col gap-2 w-full p-7'>
+        <Button variant={'outline'}>
+          <FcGoogle />
+          Login with Google
+        </Button>
+        <Button variant={'outline'}>
+          <FaGithub />
+          Login with Github
+        </Button>
+      </CardContent>
+      <DottedSeparator className='px-7 mb-2' />
+      <CardContent className='text-center p-7 text-sm'>
+        <p>已经有帐号了？<Link href={'/sign-in'} className='text-blue-500'>Sign In</Link></p>
+      </CardContent>
+    </Card>
+  )
+}
