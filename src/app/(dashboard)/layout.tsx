a38c8fab2a5fe60aacf4aa82554b60b4abb3bc64
linzheng <alidlg9045@126.com>
2024-11-02 00:11:19 +0800

    feat: dashboard - navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 
 export default function DashboardLayout({
@@ -12,11 +13,11 @@ export default function DashboardLayout({
         <div className="hidden fixed left-0 top-0 lg:block lg:w-[264px] h-full overflow-y-auto">
           <Sidebar />
         </div>
-        <div className="lg:pl-[264px]">
+        <div className="lg:pl-[264px] w-full">
           <div className="mx-auto max-w-screen-2xl h-full">
             {/* nav */}
-            <div className="h-[60px] border"></div>
-            <main>
+            <Navbar />
+            <main className="h-full py-8 px-6 flex flex-col">
               {children}
             </main>
           </div>
---
+++ src/components/navbar.tsx
@@ -0,0 +1,15 @@
+import UserButton from "@/features/auth/components/user-button";
+
+export default function Navbar() {
+  return (
+    <nav className="pt-4 px-6 flex items-center justify-between">
+      <div className="hidden lg:flex flex-col">
+        <h1 className="text-2xl font-semibold">Home</h1>
+        <p className="text-muted-foreground">show all projects and task about you</p>
+      </div>
+      <div>
+        <UserButton />
+      </div>
+    </nav>
+  )
+}
